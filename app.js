5f6a7dc0c42769283f8468bcd3c85d8ebd3a0135
rrricardo00 <rrricardo00@yahoo.com.br>
2020-01-10 00:48:05 -0300

    alpha

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 consign().include('routes').into(app);
 
 app.get('/', (req, res) => res.send('Hello World!'));
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,6 @@
 module.exports = (app) => {
 
-  app.get('/index/:nome/:lang', (req, res) => {
-    const nome = req.params.nome;
-    const lang = req.params.lang;
-  
-    res.render("index", {
-      nome,
-      lang
-    });
+  app.get('/index', (req, res) => {
+    res.render('index');
   });
-
 }
